2dce96580756c5762126b52bd30509e58344f3de
matiar_ <mateusz.sidor15@gmail.com>
2024-04-30 20:11:42 +0200

    feature: main component with routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import TopNavBar from "./components/shared/TopNavBar";
-import Greet from "./components/Greet";
-import "./App.css"
+import "./styles/App.css"
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import ProtectedRoute from "./components/shared/ProtectedRoute";
+import Home from "./components/page/Home";
+import Register from "./components/page/Register";
+import Login from "./components/page/Login";
+import NotFound from "./components/page/NotFound";
+
+function Logout() {
+    localStorage.clear();
+    return <Navigate to={"/login"}/>;
+}
+
+function RegisterAndLogout() {
+    localStorage.clear();
+    return <Register/>;
+}
 
 function App() {
     return (
-        <div className="App">
-            <TopNavBar className="TopNavBar"/>
-            <Greet name="Tomasz"/>
-            <Greet name="Łukasz"/>
-            <Greet name="Arek"/>
-            <Greet name="Mateusz"/>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>
+                }/>
+                <Route path={"/login"} element={
+                    <Login/>
+                }/>
+                <Route path={"/logout"} element={
+                    <Logout/>}
+                />
+                <Route path={"/register"} element=
+                    {<RegisterAndLogout/>}
+                />
+                <Route path={"*"} element=
+                    {<NotFound/>}
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
