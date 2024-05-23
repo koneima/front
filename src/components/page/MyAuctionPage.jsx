0a7e458238f6c8f470f8828f1d290dacd404a55b
matiar_ <mateusz.sidor15@gmail.com>
2024-05-23 20:38:55 +0200

    feature: added My auction page implementation + service method

--- src/components/page/MyAuctionPage.jsx
+++ src/components/page/MyAuctionPage.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Grid} from "@mui/material";
 import TopNavBar from "../shared/TopNavBar";
+import AuctionSearchBar from "../auction/AuctionSearchBar";
+import MyAuctionContainer from "../auction/my/MyAuctionContainer";
 
 const MyAuctionPage = () => {
+    const [filterValue, setFilterValue] = useState({});
 
     return <Box sx={{flexGrow: 1, p: 2}}>
         <Grid container
@@ -12,8 +15,11 @@ const MyAuctionPage = () => {
             <Grid item xs={12}>
                 <TopNavBar/>
             </Grid>
-            <Grid item xs={12}>
-
+            <Grid item xs={2}>
+                <AuctionSearchBar onChange={(value) => setFilterValue(value)}/>
+            </Grid>
+            <Grid item xs={10}>
+                <MyAuctionContainer filterValue={filterValue}/>
             </Grid>
         </Grid>
     </Box>
